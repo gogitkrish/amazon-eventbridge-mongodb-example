bd49eaf249fc7c910777d652d1ccd397c08df9ad
Gopalakrishnan Ramaswamy <gopal_kr272@yahoo.com>
2020-12-18 09:51:48 +0530

    Delete index.js

--- eventLambda/index.js
+++
@@ -1,5 +0,0 @@
-exports.handler = (event, context, callback) => {
-    console.log('LogScheduledEvent');
-    console.log('Received event:', JSON.stringify(event, null, 2));
-    callback(null, 'Finished');
-};
